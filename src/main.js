0512dbc4c8e65b03e3ec1cbdf6d175a968cc492c
jayjay-3 <jay_liny33520@163.com>
2022-06-21 07:29:28 +0800

    初始化并梳理项目的结构

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
